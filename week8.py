a9d89e8dd7b4792fd2fe1c1bd9815d11c6151240
00010358 <73095425+00010358@users.noreply.github.com>
2020-12-10 18:56:17 +0500

    Add files via upload

---
+++ week8.py
@@ -0,0 +1,15 @@
+#example 1
+num = {0: 1, 1: 2, 2: 3, 4: 4, 5: 5, 6: 6, 7: 7}
+
+for k, v in enumerate(num):
+    print(k, v)
+#example 2
+
+numdict = {1:'a', 2:'b', 3:'c', 4:'d'}
+
+for i, k in enumerate(numdict):
+    print(i, k)
+#example3
+numl = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for k, v in enumerate(numl):
+    print(i, j)
